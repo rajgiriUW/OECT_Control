6c970a321f7b57f2146ef29b85c897a3c4e78068
Raj Giridharagopal <rgiri@uw.edu>
2020-07-23 12:37:04 -0700

    update base.json and requirements files

--- target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'OECT Control', 'author': 'Gingerlab', 'version': '0.0.0', 'environment': 'local'}
